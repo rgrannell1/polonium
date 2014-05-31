86bfbbd0095a59f29c32befa0fe8efd5d33334cd
rgrannell1 <r.grannell2@gmail.com>
2014-05-31 12:36:38 +0100

    simplifier error checking

--- test/all.js
+++ test/all.js
@@ -1,11 +1,12 @@
 #!/usr/bin/env node
 
-const assert        = require('assert')
-const prompt        = require('prompt') // save me implementing tput bindings.
+const assert           = require('assert')
+const prompt           = require('prompt') // save me implementing tput bindings.
+const bignum           = require('bignum')
 
-const lengthInBase  = require('../lib/derive-keys').lengthInBase
-const deriveKeys    = require('../lib/derive-keys').deriveKeys
-const convertBase62 = require('../lib/derive-keys').convertBase62
+const lengthInBase     = require('../lib/derive-keys').lengthInBase
+const deriveKeys       = require('../lib/derive-keys').deriveKeys
+const convertToCharset = require('../lib/derive-keys').convertToCharset
 
 const log = console.log
 
@@ -15,22 +16,42 @@ const log = console.log
 
 ;( function () {
 
-	log("test that lengthInBase( ) is accurate".blue)
+	log("test that converting from base10.".blue)
 
-	// converting from on base to the same base preserves the length.
+	const alphanumbers =
+		'abcdefghijklmnopqrstuvwxyx' +
+		'ABCDEFGHIJKLMNOPQRSTUVWXYX' +
+		'0123456789'
 
-	for (var len = 1; len < 10000; len++) {
-		for (var base = 1; base < 1000; base++) {
 
-			var outLength = lengthInBase(len, base, base)
+	var groups = {equal: [], tosmall: [], tobig: []}
 
-			assert(
-				outLength === len,
-				('failed for (' + len + ',' + base + ',' + base + ') with the length ' + outLength + ')').red)
+	for (var ith = 0; ith < 100; ith++) {
+		var base10 = ith.toString()
 
+		var base62 = convertToCharset(base10, alphanumbers)
+		var expectLength = lengthInBase({
+			length  : base10.length,
+			fromBase: 10,
+			toBase  : 62
+		})
+
+		if ((base62.length - expectLength) === 0 ) {
+			groups.equal.push( base10 + " " + base62 + " " + expectLength )
 		}
+
+		if ((expectLength - base62.length) === -1 ) {
+			groups.tosmall.push( base10 + " " + base62 + " " + expectLength )
+		}
+		if ((expectLength - base62.length) === +1 ) {
+			groups.tobig.push( base10 + " " + base62 + " " + expectLength )
+		}
+
+
+		//console.assert(base62.length - expectLength > 1)
 	}
 
+	console.dir(groups)
 
 } )()
 
