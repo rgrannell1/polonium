dded633663cd48393b8a446ff3cd8144522af895
rgrannell1 <r.grannell2@gmail.com>
2014-06-01 14:24:48 +0100

    reformatted comments

--- test/all.js
+++ test/all.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 const assert           = require('assert')
-const prompt           = require('prompt') // save me implementing tput bindings.
+const prompt           = require('prompt') // -- save me implementing tput bindings.
 const bignum           = require('bignum')
 
 const deriveKeys       = require('../lib/derive-keys').deriveKeys
