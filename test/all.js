09be46bf0d82fb317b2b4f80eb35d9f2f99657d4
rgrannell1 <r.grannell2@gmail.com>
2014-06-01 02:27:16 +0100

    removed lengthinbase

--- test/all.js
+++ test/all.js
@@ -4,7 +4,6 @@ const assert           = require('assert')
 const prompt           = require('prompt') // save me implementing tput bindings.
 const bignum           = require('bignum')
 
-const lengthInBase     = require('../lib/derive-keys').lengthInBase
 const deriveKeys       = require('../lib/derive-keys').deriveKeys
 const convertToCharset = require('../lib/derive-keys').convertToCharset
 
@@ -16,31 +15,7 @@ const log = console.log
 
 ;( function () {
 
-	log("test that converting from base10.".blue)
 
-	for (var masterNum = 0; masterNum < 1000; masterNum++) {
-
-		var master = masterNum.toString()
-
-		for (var rounds = 0; rounds < 100; rounds++) {
-
-			for (var saltNum = 0; saltNum < 100; saltNum++) {
-
-				var salt = saltNum.toString()
-
-				for (var len = 0; len < 100; len++) {
-
-					var out = deriveKeys({
-						master: master,
-						rounds : rounds,
-						salt  : salt,
-						len   : len
-					})
-
-				}
-			}
-		}
-	}
 
 
 
