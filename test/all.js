c6b0cafe2035a1df4b5a7d44945866926d0ab638
rgrannell1 <r.grannell2@gmail.com>
2014-06-04 22:19:33 +0100

    moving to 0.1.0

--- test/all.js
+++ test/all.js
@@ -1,22 +1,2 @@
 #!/usr/bin/env node
 
-const assert           = require('assert')
-const prompt           = require('prompt') // -- save me implementing tput bindings.
-const bignum           = require('bignum')
-
-const deriveKeys       = require('../lib/derive-keys')
-
-const log = console.log
-
-
-
-
-
-;( function () {
-
-
-
-
-
-} )()
-
